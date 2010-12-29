3f9d2b7600b187a036bd1c53c73c86e657a6326d
Rich <rich@Rich-Mac.local>
2010-12-29 00:14:13 +0000

    Fixed toggling on/off

--- includes/firebuglite.js
+++ includes/firebuglite.js
@@ -3,38 +3,38 @@
 // @include https://*
 // ==/UserScript==
 
-(function(window, document, undefined) {
-	
-	function toggleFirebug() {
-		var firebugEl = document.getElementById('FirebugLite');
-		if(firebugEl) {
-			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).removeChild(firebugEl);
-			updateStatus(false);
-		} else {
-			firebugEl = document['createElementNS'] && document.documentElement.namespaceURI;
-			firebugEl = firebugEl ? document.createElementNS(firebugEl, 'script') : document.createElement('script');
-			firebugEl.setAttribute('id', 'FirebugLite');
-			firebugEl.setAttribute('src', 'https://getfirebug.com/firebug-lite.js#startOpened');
-			firebugEl.setAttribute('FirebugLite', '4');
-			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(firebugEl);
-			updateStatus(true);
-		}
-	}
-	
-	function updateStatus(isActive) {
-		opera.extension.postMessage({
-			action: 'updateStatus',
-			status: isActive ? '1' : '0'
-		});
-	}
-	
-	opera.extension.addEventListener( 'message', function( msg ) {
-		if(msg.data.action == 'toggleFirebug' || msg.data.action == 'autoRun')
-			toggleFirebug();
-	}, false);
-	
-	opera.extension.postMessage({
-		action: 'load'
-	});
+var active = false;
 
-}(window, document));
+function toggleFirebug() {
+	var firebugEl = document.getElementById('FirebugLite');
+	if( firebugEl ) {
+		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).removeChild(firebugEl);
+		updateStatus(false);
+	} else {
+		firebugEl = document.createElementNS && document.documentElement.namespaceURI;
+		firebugEl = firebugEl ? document.createElementNS	(firebugEl, 'script') : document.createElement('script');
+		firebugEl.setAttribute('id', 'FirebugLite');
+		firebugEl.setAttribute('src', 'https://getfirebug.com/firebug-lite.js#startOpened');
+		firebugEl.setAttribute('FirebugLite', '4');
+		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(firebugEl);
+		firebugEl = new Image;
+		firebugEl.setAttribute('src', 'https://getfirebug.com/releases/lite/latest/skin/xp/sprite.png');
+		updateStatus(true);
+	}
+}
+
+function updateStatus(isActive) {
+	opera.extension.postMessage({
+		action: 'updateStatus',
+		status: isActive ? '1' : '0'
+	});
+}
+
+opera.extension.addEventListener( 'message', function( msg ) {
+	if(msg.data.action == 'toggleFirebug' || msg.data.action == 'autoRun')
+		toggleFirebug();
+}, false);
+
+opera.extension.postMessage({
+	action: 'load'
+});
